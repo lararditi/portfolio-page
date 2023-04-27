0e8f3e1a74b23c73f8a2124c956734e199f8cfea
lararditi <ardil219@newschool.edu>
2023-04-27 09:14:53 -0400

    git

--- global.js
+++ global.js
@@ -1,3 +1,46 @@
+
+
+const modalContentData = [
+    {
+        title: "KOSHER NYC",
+        description: "A hypothetical exhibition/food tour which uses New York city as its space which takes inspiration in its form from the grid maps apps are built on. ",
+        img: ['onemap.jpg', 'frontback.jpg', 'two.jpg', 'three.jpg'],
+        icon: ['./portforlio-page/porkosher.png']
+    },
+    {
+        title: "HOME",
+        description: "This booklet draws the relationship between the dimensions the word “home” is expressed in - the visual symbols we universally use to demonstrate “home”, the spaces we call “home” and what alternatives to what “home” can be to individuals. This is not only expressed through the content but also literally expressed through the use of a changing grid in the background of each section.",
+        img: ['h.jpg', 'home.jpg', 'home-1.jpg', 'home-2.jpg', 'home-3.jpg', 'home4.jpg'],
+        icon: ['']
+    },
+    {
+        title: "ELEMENTS OF THE PERIODIC TABLE",
+        description: "This poster emphasizes grid systems and typographic hierarchy not only to divide the page, but also by using units created by it to show the numerical value of atomic weights visually. By using the divisibility of the grid the design invites the viewer to zoom in and out and to think about how atoms build the objects we see.",
+        img: ['table.jpg', 'bi.jpg'],
+        icon: ['']
+    },
+    {
+        title: "FRUIT STICKERS",
+        description: "This website's purpose is to explore using API keys and filter a collection of fruit sticker based on companies' ethics: url.",
+        img: ['periodic-table.jpg'],
+        icon: ['']
+    },
+
+    {
+        title: "TUNNEL",
+        description: "This website is constructed to create a narrative by using multiple platforms.",
+        img: ['./periodic-table.jpg'],
+        icon: ['kosher.png']
+    },
+    {
+        title: "NYC FOODSCRAP DROPOFF LOCATIONS",
+        description: "This site showcases information from NYC Open Data and is made to learn the basics of JavaScript. The user can see the density of food scrap dropoff locations as it it is displayed by using a grayscale and can get more information on the adress through interaction.",
+        img: ['hero.jpg', 'blue.jpg'],
+        icon: ['']
+    },
+
+]
+
 let totalProjectCount = 6
 let projectContainer = document.getElementById('project-container')
 let modal = document.getElementById('modal');
@@ -5,7 +48,7 @@ for (let projectCount = 0; projectCount < totalProjectCount; projectCount++) {
     let currentDiv = document.createElement('div');
     currentDiv.id = `project${projectCount}`;
     currentDiv.dataset.number = projectCount
-    currentDiv.innerHTML = `<div class="core-1"> <img data-number="${projectCount}" src="${modalContentData[projectCount].icon}"> </div>`
+    currentDiv.innerHTML = `<div class="core-1"> <img data-number="${projectCount}" src="${modalContentData[projectCount].img}"> </div>`
     projectContainer.appendChild(currentDiv);
     currentDiv.addEventListener('click', function(event) {
         let selectedProject = event.target.getAttribute('data-number')
@@ -18,42 +61,6 @@ modal.addEventListener('click', function() {
         modal.style.display = 'none'
     
 })
-
-const modalContentData = [
-    {
-        title: "KOSHER NYC",
-        description: "A hypothetical exhibition/food tour which uses New York city as its space which takes inspiration in its form from the grid maps apps are built on. ",
-        img: ['onemap.jpg', 'frontback.jpg', 'two.jpg', 'three.jpg'],
-        icon: 'koshericon.jpg'
-    },
-    {
-        title: "HOME",
-        description: "This booklet draws the relationship between the dimensions the word “home” is expressed in - the visual symbols we universally use to demonstrate “home”, the spaces we call “home” and what alternatives to what “home” can be to individuals. This is not only expressed through the content but also literally expressed through the use of a changing grid in the background of each section.",
-        img: ['hero.jpg', 'blue.jpg']
-    },
-    {
-        title: "ELEMENTS OF THE PERIODIC TABLE",
-        description: "This poster emphasizes grid systems and typographic hierarchy not only to divide the page, but also by using units created by it to show the numerical value of atomic weights visually. By using the divisibility of the grid the design invites the viewer to zoom in and out and to think about how atoms build the objects we see.",
-        img: ['table.jpg', 'bi.jpg']
-    },
-    {
-        title: "FRUIT STICKERS",
-        description: "This website's purpose is to explore using API keys and filter a collection of fruit sticker based on companies' ethics: url.",
-        img: ['periodic-table.jpg']
-    },
-
-    {
-        title: "TUNNEL",
-        description: "This website is constructed to create a narrative by using multiple platforms.",
-        img: ['./periodic-table.jpg']
-    },
-    {
-        title: "NYC FOODSCRAP DROPOFF LOCATIONS",
-        description: "This site showcases information from NYC Open Data and is made to learn the basics of JavaScript. The user can see the density of food scrap dropoff locations as it it is displayed by using a grayscale and can get more information on the adress through interaction.",
-        img: ['hero.jpg', 'blue.jpg']
-    },
-
-]
 
 
 function renderModal(projectNumber) {
