88200c50db66eedaa109c5abf8f1906c65d28fb1
lararditi <ardil219@newschool.edu>
2023-05-01 18:05:13 -0400

    saveit

--- global.js
+++ global.js
@@ -17,27 +17,27 @@ const modalContentData = [
         title: "ELEMENTS OF THE PERIODIC TABLE",
         description: "This poster emphasizes grid systems and typographic hierarchy not only to divide the page, but also by using units created by it to show the numerical value of atomic weights visually. By using the divisibility of the grid the design invites the viewer to zoom in and out and to think about how atoms build the objects we see.",
         img: ['table.jpg', 'bi.jpg'],
-        icon: ['']
+        icon: ['bi.jpg']
     },
     {
         title: "FRUIT STICKERS",
         description: "This website's purpose is to explore using API keys and filter a collection of fruit sticker based on companies' ethics. <br> Click on link for website: ",
         img: ['fruit.png'],
-        icon: ['']
+        icon: ['fruit.png']
     },
 
     {
         title: "TUNNEL",
         description: "This website is constructed to create a narrative by using multiple platforms. <br> Click on link for website: ",
         img: ['sketch-tunnel.png'],
-        icon: ['']
+        icon: ['sketch-tunnel.png']
     },
     {
         title: "NYC FOODSCRAP DROPOFF LOCATIONS",
         description: "This site showcases information from NYC Open Data and is made to learn the basics of JavaScript. The user can see the density of food scrap dropoff locations as it it is displayed by using a grayscale and can get more information on the adress through interaction. <br> Click on link for website: ",
         img: ['fooddrop.png'],
-        icon: ['']
-    },
+        icon: ['fooddrop.png']
+    }
 
 ]
 
@@ -52,22 +52,19 @@ for (let projectCount = 0; projectCount < totalProjectCount; projectCount++) {
     projectContainer.appendChild(currentDiv);
     currentDiv.addEventListener('click', function(event) {
         let selectedProject = event.target.getAttribute('data-number')
-        renderModal(selectedProject)
+        renderModal(modalContentData[selectedProject], Number(selectedProject))
         console.log(selectedProject)
     })
 }
 
-modal.addEventListener('click', function() {
-        modal.style.display = 'none'
-    
-})
+let closeButton = document.querySelector("#closeButton")
 
-
-function renderModal(projectNumber) {
-    let clickedProject = modalContentData[projectNumber]
+function renderModal(clickedProject, selectedProject) {
+    console.log(clickedProject,selectedProject);
     modal.innerHTML = `
     <div class="about">${clickedProject.title}</div>
     <div class="info">${clickedProject.description}</div>
+    
     
     `
     for (image of clickedProject.img) {
@@ -78,5 +75,39 @@ function renderModal(projectNumber) {
     }
     
     modal.style.display = 'block'
+
+    let closeButton = document.createElement("h2");
+    closeButton.innerHTML="X";
+    closeButton.id = "closeButton";
+    closeButton.addEventListener('click', function() {
+        modal.style.display = 'none'
+    })
+    let buttonContainer = document.createElement("div");
+    let next = document.createElement("button");
+    let prev = document.createElement("button");
+    buttonContainer.classList.add("btn-container");
+    next.innerHTML = ">";
+    prev.innerHTML = "<";
+    next.addEventListener('click', ()=>{
+        if(selectedProject+1>=modalContentData.length){
+            renderModal(modalContentData[0], 0)
+        }else{
+            renderModal(modalContentData[selectedProject+1], selectedProject+1);
+        }
+    });
+
+    prev.addEventListener('click', ()=>{
+        if(selectedProject-1<0){
+            renderModal(modalContentData[modalContentData.length-1], modalContentData.length-1)
+        }else{
+            renderModal(modalContentData[selectedProject-1], selectedProject-1);
+        }
+    });
+
+    buttonContainer.append(prev, next);
+    modal.append(closeButton, buttonContainer);
 }
+
+
+
 
